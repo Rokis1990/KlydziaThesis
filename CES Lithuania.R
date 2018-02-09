7295ea6fecc10e686281f082b32686cd9014b10e
Rokas Klydžia <rokas.klydzia@gmail.com>
2018-02-09 07:11:55 +0200

    Pasistūmėjau bet užstrigau ties parametrų parinkimu naudojant nls() funkciją
    Pakeista duomenų transformacija (gali tekti atkeisti atgal) - logaritmuoti duomenys nebepadauginti iš 100.
    Sukurta stackoverflow užklausa. praeita nemažai kodo, tačiau nepaskaičiuota nei viena gamybos funkcija.
    Kol kas dirbta tik su dviem nepriklausomais kintamaisiais: darbu ir kapitalu (SEMPh_LT ir GFCF_LT).

--- CES Lithuania.R
+++ CES Lithuania.R
@@ -1,19 +1,20 @@
 library(micEconCES)
 
+#<PVZ>
 a<-data("MishraCES")
 a
 colMeans(MishraCES)
 b<-data("GermanIndustry")
-
-data <- read.delim(("~/R/ekonometrijaPROJ/data.txt"))
+#</PVZ>
+#<TRIAL>
+data <- read.delim(("//hnas-cifs.activedir.vu.lt/CIFS_User/Redirected_studentai/s1716296/Documents/KlydziaThesis/data/data.txt"))
 attach(data)
-
-data_ol <- read.delim("https://raw.githubusercontent.com/Rokis1990/KlydziaThesis/master/data/data.txt") # tik pademonstravimui kaip ištraukti online txt failus. Githube reik eiti į raw direktoriją.
-y0 <- ts(log(GVA_LT)*100, start=c(1999, 1), frequency=4)
+data_ol <- read.delim("https://raw.githubusercontent.com/Rokis1990/KlydziaThesis/master/data/data.txt")
+y0 <- ts(log(GVA_LT), start=c(1999, 1), frequency=4)
 y <- y0 - decompose(y0)$seasonal
-l0 <- ts(log(SEMPh_LT)*100, start=c(1999, 1), frequency=4)
+l0 <- ts(log(SEMPh_LT), start=c(1999, 1), frequency=4)
 l <- l0 - decompose(l0)$seasonal
-k0 <- ts(log(GFCF_LT)*100, start=c(1999, 1), frequency=4)
+k0 <- ts(log(GFCF_LT), start=c(1999, 1), frequency=4)
 k <- k0 - decompose(k0)$seasonal
 dy <- diff(y)
 dl <- diff(l)
@@ -22,23 +23,98 @@ plot.ts(dy)
 plot.ts(dl)
 plot.ts(dk)
 ??MicEconCES
+#</TRIAL>
 
-
+#<PVZ>
 set.seed( 123 )
 cesData <- data.frame(x1 = rchisq(200, 10), x2 = rchisq(200, 10), x3 = rchisq(200, 10), x4 = rchisq(200, 10) )
 cesData$y2 <- cesCalc( xNames = c( "x1", "x2" ), data = cesData, coef = c( gamma = 1, delta = 0.6, rho = 0.5, nu = 1.1 ) )
+#</PVZ>
+#<TRIAL>
+#<Testing water>
+attach(cesData)
+ts.plot(x2)
+ts.plot(y2)
+#<RAMBLING>
+#jei pavyzdyje naudoja pirmus skirtumus, reiškia ir aš turiu transformuoti savo raw data į diferencijuotus duomenis. 
+#tai jau padariau, tačiau kad nusezoninčiau teko panaudoti ts(), o po to decompose() komandą. Bet dėl to mano kintamieji y, y0, k, k0, etc tapo laiko eilučių kintamaisiais.
+# Ir galbūt dėl to, kad jie nėra "pliki" duomenys, o laiko eilutės, jų nenori gražiai nuskaityti micEconCES paketo komandos.
+#</RAMBLING>
+LTTrialData <- data.frame(x1 = l, x2 = k)
+ts.plot(dl) #mano duomenys
+ts.plot(x1) #simuliuoti duomenys
+ts.plot(dk) #mano duomenys
+ts.plot(y2) #simuliuoti duomenys
+#<RAMBLE>
+#nesuprantu šitų simuliuotų duomenų esmės. jų skirstinys lyg ir normalus. visi jie teigiami. 
+#dabargi, kai imu savo realius duomenis, jie yra ir logaritmuoti, ir diferencijuoti. 
+# Jei aš savo duomenis vien logaritmuočiau, 
+k
+l
+# tai jie vis tiek išliktų dideli. Bet logaritmavimas reikalingas tik tam, kad gerai aproksimuotų ir padėtų linearizuoti funkciją.
+#tik, logaritmuoti duomenys toli gražu ne normaliai pasiskirstę
+plot(l)
+plot(k)
+plot(y)
+plot(GVA_LT)
+plot(SEMPh_LT)
+plot(GFCF_LT)
+#svarbu išlaikome formą
+#bet tai kodėl tuomet pavyzdyje pasirinkti tokie pastovūs duomenys? Beveik norisi diferencijuoti savo duomenis, bet juk diferencijavęs savo duomenis gaučiau tiesiog pirmus skirtumus, beveik neišvengiamai su nuliniu vidurkiu.
+#reiks išmėginti įstatyti ir diferencijuotus kintamuosius į gamybos funkciją dl, dk, dy, ir tiesiog logaritmuotus l, k, y.
+#</Ramble>
+LTTrialData$y2 <- y
+#</TRIAL>
 
-
+#<PVZ>
 cesData$y2 <- cesData$y2 + 2.5 * rnorm( 200 )
 cesData$y3 <- cesCalc(xNames = c("x1", "x2", "x3"), data = cesData,
                       coef = c( gamma = 1, delta_1 = 0.7, delta = 0.6, rho_1 = 0.3, rho = 0.5,
                                 nu = 1.1), nested = TRUE )
+#<RAMBLE>
+# kodėl jie pavyzdyje iš pradžių suskaičiuoja y3 iš 3 inputų (x1, x2, x3), o vėliau ima ir...
+# prideda normaliai pasiskirsčiusį nuokrypį (+ 1.5) ?
+
+# Kitas svarbus klausimas - IŠ KUR JIE IMA IR IŠTRAUKIA KOEFICIENTUS REIKALINGUS cesCalc() funkcijai???
+# Tai yra, iš kur jie "žino" kokios turi būti reikšmės gama, delta_1, delta, rho_1, rho ir nu?
+# Ar šie skaičiai gaunami iš indėlių santykinių kiekių išreikštų pinigais (wage share, profit share, etc)?
+#</RAMBLE>
+
 cesData$y3 <- cesData$y3 + 1.5 * rnorm(200)
 cesData$y4 <- cesCalc(xNames = c("x1", "x2", "x3", "x4"), data = cesData,
                       coef = c(gamma = 1, delta_1 = 0.7, delta_2 = 0.6, delta = 0.5,
                                rho_1 = 0.3, rho_2 = 0.4, rho = 0.5, nu = 1.1), nested = TRUE )
 cesData$y4 <- cesData$y4 + 1.5 * rnorm(200)
+#<RAMBLE>
+# kol kas neturiu tikrų duomenų trečiam ir ketvirtam x, bet vėliau matyt įtrauksiu, galbūt kaip žmogiškojo kapitalo rodiklį iš Barro-Lee duombazės
+#</RAMBLE>
+#</PVZ>
 
+
+# N
+
+#<TRIAL>
+cesNlsLT <- nls( y2 ~ gamma * ( delta * x1^(-rho) + (1 - delta) * x2^(-rho) )^(-phi / rho),
+  data = LTTrialData, start = c(gamma = 0.5, delta = 0,5, rho = 0.25, phi = 1))
+# kadangi metė klaida "Error in start[[i]] : subscript out of bounds"
+# pamėginau iš savo logaritmuotų duomenų išmesti *100, nes vis tiek ne procentinės ten išraiškos, o šimtai ir apie tūkstantį gaunasi padauginus iš 100.
+#dabar reikia kelis kintamuosius "pragroti" iš naujo ir pamėginti viršuje esančią operaciją dar kartą.
+
+# pakeisti duomenis NEPADĖJO
+# Stackexchange siūlo naudotis debuginimo funkcija 
+# >options(error=recover)
+# > <funkcija iššaukusi klaidą>
+# > 1
+# >ls()
+# 
+ # "algorithm"   "cl"          "control"     "data"        "env"         "form2"       "formula"     "lenVar"     
+#[9] "lower"       "mf"          "model"       "mWeights"    "n"           "na.action"   "not.there"   "pnames"     
+#[17] "respLength"  "start"       "subset"      "trace"       "upper"       "var"         "varIndex"    "varNames"   
+#[25] "varNamesRHS" "weights"     "wts"        
+#Browse[1]> data
+
+# problemos lengvai nesisprendžia, padariau užklausą stackexchange
+# https://stackoverflow.com/questions/48698979/subscript-out-of-bounds-error-in-miceconces-nls-function
 
 cesNls <- nls( y2 ~ gamma * ( delta * x1^(-rho) + (1 - delta) * x2^(-rho) )^(-phi / rho),
                data = cesData, start = c( gamma = 0.5, delta = 0.5, rho = 0.25, phi = 1 ) )
